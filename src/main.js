7f663e4ee3aa330dc140fc218940e73ae860eb05
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-07-28 16:48:43 +0300

    Добавляет рендер карточек на сайт

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import {createUserTemplate} from './view/user';
 import {createMenuTemplate} from './view/menu';
 import {createSortTemplate} from './view/sort';
 import {createCardTemplate} from "./view/card";
+import {createFilmsConteinerTemplate} from './view/films';
 
 const header = document.querySelector('.header');
 const main = document.querySelector('.main');
-
-
+const CART_AMOUNT = 5;
+const CART_AMOUNT_EXTRA = 2;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -15,4 +16,17 @@ const render = (container, template, place) => {
 render(header, createUserTemplate(), 'beforeend');
 render(main, createMenuTemplate(), 'beforeend');
 render(main, createSortTemplate(), 'beforeend');
-//render(filmsList, createCardTemplate(), 'beforeend');
+render(main, createFilmsConteinerTemplate(), 'beforeend');
+
+const filmsListContainers = document.querySelectorAll('.films-list__container');
+const [allMovies, ...extraMovies] = filmsListContainers;
+
+for (let i = 0; i < CART_AMOUNT; i++) {
+  render(allMovies, createCardTemplate(), 'beforeend');
+}
+
+extraMovies.forEach((container) => {
+  for (let i = 0; i < CART_AMOUNT_EXTRA; i++) {
+    render(container, createCardTemplate(), 'beforeend');
+  }
+});

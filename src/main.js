1b670a60683b96a3e0fd67effe16fbaff3bc9fae
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-08-09 13:30:16 +0300

    Рефакторит функцию допоказа карточек

--- src/main.js
+++ src/main.js
@@ -11,15 +11,13 @@ import {getRandomInteger, renderElement, RenderPosition} from './utils';
 
 const header = document.querySelector('.header');
 const main = document.querySelector('.main');
-const CART_AMOUNT = 20;
+const CART_AMOUNT = 6;
 const CART_START = 5;
 const CART_ADDED = 5;
 const CART_AMOUNT_EXTRA = 2;
 const body = document.body;
 
 const filmsArray = new Array(CART_AMOUNT).fill().map(createFilm);
-const lastFilmsArrayIndex = filmsArray.length - 1;
-let lastShownFilmIndex = CART_START - 1;
 
 
 renderElement(header, new UserView().getElement(), RenderPosition.BEFOREEND);
@@ -55,15 +53,11 @@ const renderFilm = (filmsContainer, film) => {
     popupComponent.getElement().querySelector('.film-details__close-btn').addEventListener('click', onPopupClose);
   }
 
-  [ cardComponent.getElement().querySelector('img'),
+  [cardComponent.getElement().querySelector('img'),
     cardComponent.getElement().querySelector('h3'),
     cardComponent.getElement().querySelector('.film-card__comments'),
   ].forEach((item) => item.addEventListener('click', onOpenPopup));
 };
-
-for (let i = 0; i < Math.min(filmsArray.length, CART_START); i++) {
-  renderFilm(allMovies, filmsArray[i]);
-}
 
 extraMovies.forEach((container) => {
   for (let i = 0; i < CART_AMOUNT_EXTRA; i++) {
@@ -71,19 +65,27 @@ extraMovies.forEach((container) => {
   }
 });
 
-renderElement(filmsList, new ShowMoreView().getElement(), RenderPosition.BEFOREEND);
 renderElement(footerStatistics, new FilmsAmountView(CART_AMOUNT).getElement());
-const btnShowMore = document.querySelector('.films-list__show-more');
-btnShowMore.addEventListener('click', onBtnShowMoreClick);
 
-function onBtnShowMoreClick(evt) {
-  evt.preventDefault();
-  const max = lastShownFilmIndex + CART_ADDED >= lastFilmsArrayIndex ? (lastFilmsArrayIndex - lastShownFilmIndex) : CART_ADDED;
-  for (let i = 0, j = lastShownFilmIndex; i < max; i++, j++) {
-    renderElement(allMovies, new CardView(filmsArray[j]).getElement(), RenderPosition.BEFOREEND);
-  }
-  lastShownFilmIndex += CART_ADDED;
-  if (lastShownFilmIndex === lastFilmsArrayIndex) {
-    evt.target.remove();
-  }
+for (let i = 0; i < Math.min(filmsArray.length, CART_START); i++) {
+  renderFilm(allMovies, filmsArray[i]);
 }
+
+if (filmsArray.length > CART_START) {
+  let lastShownFilmNumber = CART_START;
+  const btnShowMore = new ShowMoreView().getElement();
+  renderElement(filmsList, btnShowMore, RenderPosition.BEFOREEND);
+
+  btnShowMore.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    const NumberOfAddedCard = lastShownFilmNumber + CART_ADDED >= filmsArray.length ? (filmsArray.length - lastShownFilmNumber) : CART_ADDED;
+    filmsArray.slice(lastShownFilmNumber, lastShownFilmNumber + NumberOfAddedCard).forEach((film) => {
+      renderFilm(allMovies, film);
+      lastShownFilmNumber++;
+    });
+    if(lastShownFilmNumber === filmsArray.length){
+      btnShowMore.remove();
+    }
+  });
+}
+

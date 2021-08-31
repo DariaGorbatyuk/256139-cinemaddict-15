0ee1739a32fee69dfe2e935817f97d76cf54b36a
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-08-31 15:50:26 +0300

    Добавляет пока непонятно что

--- src/presenter/film.js
+++ src/presenter/film.js
@@ -1,33 +1,54 @@
 import CardView from '../view/card';
 import PopupView from '../view/popup';
-import {renderElement, RenderPosition, remove} from '../utils/render';
+import {renderElement, RenderPosition, remove, replace} from '../utils/render';
 
 export default class Film {
   constructor(filmContainer) {
     this._filmContainer = filmContainer;
+    this._cardComponent = null;
+    this._popupComponent = null;
     this._handlerOpenPopup = this._handlerOpenPopup.bind(this);
     this._handlerClosePopup = this._handlerClosePopup.bind(this);
   }
 
   init(film) {
     this._film = film;
-    this.cardComponent = new CardView(this._film);
-    this.popupComponent = new PopupView(this._film);
-    this.cardComponent.setOpenPopupHandler(this._handlerOpenPopup);
-    renderElement(this._filmContainer, this.cardComponent, RenderPosition.BEFOREEND);
+    const prevCardComponent = this._cardComponent;
+    const prevPopupComponent = this._popupComponent;
+
+    this._cardComponent = new CardView(film);
+    this._popupComponent = new PopupView(this._film);
+
+    this._cardComponent.setOpenPopupHandler(this._handlerOpenPopup);
+    if (prevCardComponent === null) {
+      renderElement(this._filmContainer, this._cardComponent, RenderPosition.BEFOREEND);
+      return;
+    }
+
+    if (this._filmContainer.getElement().contains(prevCardComponent.getElement())) {
+      replace(this._cardComponent, prevCardComponent);
+    }
+
+    remove(prevCardComponent);
+    remove(prevPopupComponent);
+  }
+
+  destroy() {
+    remove(this._cardComponent);
+    remove(this._popupComponent);
   }
 
   _handlerOpenPopup() {
-    if (document.body.lastElementChild.matches('.film-details')) {
-      document.body.lastElementChild.remove();
-    }
+    // if (document.body.contains(this._popupComponent.getElement())) {
+    //   this._popupComponent.remove();
+    // }
     document.body.classList.add('hide-overflow');
-    this.popupComponent.setPopupCloseHandler(this._handlerClosePopup);
-    renderElement(document.body, this.popupComponent, RenderPosition.BEFOREEND);
+    this._popupComponent.setPopupCloseHandler(this._handlerClosePopup);
+    renderElement(document.body, this._popupComponent, RenderPosition.BEFOREEND);
   }
 
   _handlerClosePopup() {
-    remove(this.popupComponent);
+    remove(this._popupComponent);
     document.body.classList.remove('hide-overflow');
   }
 }

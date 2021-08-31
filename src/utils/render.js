c00e671a2fd524876749737347961b0c7f8a846c
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-08-31 15:49:21 +0300

    Делает функцию replace

--- src/utils/render.js
+++ src/utils/render.js
@@ -37,4 +37,22 @@ const remove = (component) => {
   component.getElement().remove();
   component.removeElement();
 };
-export {renderElement, createElement, RenderPosition, remove};
+
+const replace = (newChild, oldChild) => {
+  if (oldChild instanceof Abstract) {
+    oldChild = oldChild.getElement();
+  }
+
+  if (newChild instanceof Abstract) {
+    newChild = newChild.getElement();
+  }
+
+  const parent = oldChild.parentElement;
+
+  if (parent === null || oldChild === null || newChild === null) {
+    throw new Error('Can\'t replace unexisting elements');
+  }
+
+  parent.replaceChild(newChild, oldChild);
+};
+export {renderElement, createElement, RenderPosition, remove, replace};

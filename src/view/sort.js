97c24e3d0c8289005345d7b028fc25c59a4795de
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-09-02 12:28:04 +0300

    Реализует подписку на изменения сортировки

--- src/view/sort.js
+++ src/view/sort.js
@@ -7,8 +7,26 @@ const createSortTemplate = () => (`<ul class="sort">
     <li><a href="#" class="sort__button" data-sort-type="${SortType.BY_RATING}">Sort by rating</a></li>
   </ul>`);
 
-export default class SortMenu extends AbstractView{
+export default class SortMenu extends AbstractView {
+  constructor() {
+    super();
+    this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
+  }
+
   getTemplate() {
     return createSortTemplate();
+  }
+
+  _sortTypeChangeHandler(evt) {
+    if (evt.target.tagName !== 'A') {
+      return;
+    }
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  }
+
+  setSortType(callback) {
+    this._callback.sortTypeChange = callback;
+    this.getElement().addEventListener('click', this._sortTypeChangeHandler);
   }
 }
